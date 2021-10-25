dba5b29a08ea25c5c3eb103b63037b04a5ea8ce9
Sergey Glushchenko <glushchenko2003@gmail.com>
2021-10-25 19:10:15 +0800

    update script

--- Scripts/update_configurations_from_private.sh
+++ Scripts/update_configurations_from_private.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SOURCES_DIR="./Configs"
-CONFIG_DIR="./private/Configuration"
+CONFIG_DIR="./private/Configurations"
 if [ -d "$CONFIG_DIR" ]; then
     cp -a "$CONFIG_DIR/." $SOURCES_DIR
 fi
